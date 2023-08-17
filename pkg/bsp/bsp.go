f2965be28ea7cbf8a125f02dec4e992d7ecdea67
Richard <wangweijia2000@sina.com>
2023-08-17 06:35:01 +0000

    Added telemetry update to thingsboard server.
    Signed-off-by: Richard <wangweijia2000@sina.com>

--- pkg/bsp/bsp.go
+++ pkg/bsp/bsp.go
@@ -38,41 +38,51 @@ var virtualBsp = VirtualBsp{
 	},
 }
 
+func (virtualBsp VirtualBsp) SafeUploadTelemetry(nodeId string, data interface{}) {
+	virtualBsp.thingsboardServer.CreateDevice(nodeId)
+	virtualBsp.thingsboardServer.UploadTelemetry(nodeId, data)
+}
+
 func (virtualBsp VirtualBsp) SetModule0Params(moduleParams shared.Module) {
 	moduleParamsInJson, _ := json.Marshal(moduleParams)
-	virtualBsp.thingsboardServer.CreateDevice(BspConfigInstance.GatewayNodeID)
 	fmt.Println("Setting Module0 Params", moduleParamsInJson)
+
 	data := map[string]interface{}{
 		"module0": fmt.Sprintf("band: %d, factor: %d, freq: %d",
 			moduleParams.Band, moduleParams.Factor, moduleParams.Freq),
 	}
-	virtualBsp.thingsboardServer.UploadTelemetry(BspConfigInstance.GatewayNodeID, data)
+	virtualBsp.SafeUploadTelemetry(BspConfigInstance.GatewayNodeID, data)
 }
 
 func (virtualBsp VirtualBsp) SetModule1Params(moduleParams shared.Module) {
 	moduleParamsInJson, _ := json.Marshal(moduleParams)
 	fmt.Printf("Setting Module1 Params: %s\n", moduleParamsInJson)
-	virtualBsp.thingsboardServer.CreateDevice(BspConfigInstance.GatewayNodeID)
+
 	data := map[string]interface{}{
 		"module1": fmt.Sprintf("band: %d, factor: %d, freq: %d",
 			moduleParams.Band, moduleParams.Factor, moduleParams.Freq),
 	}
-	virtualBsp.thingsboardServer.UploadTelemetry(BspConfigInstance.GatewayNodeID, data)
+	virtualBsp.SafeUploadTelemetry(BspConfigInstance.GatewayNodeID, data)
 }
 
 func (virtualBsp VirtualBsp) SetModule2Params(moduleParams shared.Module) {
 	moduleParamsInJson, _ := json.Marshal(moduleParams)
 	fmt.Printf("Setting Module2 Params: %s\n", moduleParamsInJson)
-	virtualBsp.thingsboardServer.CreateDevice(BspConfigInstance.GatewayNodeID)
+
 	data := map[string]interface{}{
 		"module2": fmt.Sprintf("band: %d, factor: %d, freq: %d",
 			moduleParams.Band, moduleParams.Factor, moduleParams.Freq),
 	}
-	virtualBsp.thingsboardServer.UploadTelemetry(BspConfigInstance.GatewayNodeID, data)
+	virtualBsp.SafeUploadTelemetry(BspConfigInstance.GatewayNodeID, data)
 }
 
 func (virtualBsp VirtualBsp) SetSingleGlassColor(nodeId string, color string) {
 	fmt.Println("Setting glass color:", nodeId, color)
+	
+	data := map[string]interface{}{
+		"color": color,
+	}
+	virtualBsp.SafeUploadTelemetry(nodeId, data)
 }
 
 func (virtualBsp VirtualBsp) SetGlassColorsBlocking(
@@ -82,6 +92,7 @@ func (virtualBsp VirtualBsp) SetGlassColorsBlocking(
 		virtualBsp.SetSingleGlassColor(value.NodeID, value.Color)
 	}
 	time.Sleep(90 * time.Second)
+
 	var reply shared.UpdateGlassColorReply
 
 	reply.GatewayNodeID = BspConfigInstance.GatewayNodeID
