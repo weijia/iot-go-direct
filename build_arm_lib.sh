9030ecd13c99f1b7469798e70759b453f075112d
weijiwan <weijiwan@cisco.com>
2024-07-03 09:44:19 +0800

    Update build script

--- build_arm_lib.sh
+++ build_arm_lib.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-path
-ls
-ls /sysroot/linux/armhf/usr/lib/arm-linux-gnueabihf/
-ls /sysroot/linux/armhf/opt/
-find . arm-linux-gnueabihf-gcc
+
 export CC=arm-linux-gnueabihf-gcc
-arm-linux-gnueabihf-gcc -c -o pan3028.o pan3028-app\pan3028.c 
-arm-linux-gnueabihf-gcc -c -o pan3028_port.o pan3028-app\pan3028_port.c
-arm-linux-gnueabihf-gcc -c -o spi-3028.o pan3028-app\spi-3028.c 
-arm-linux-gnueabihf-gcc -c -o radio.o pan3028-app\radio.c
-rem .a must be in the same folder as the go file really using the .a lib
-arm-linux-gnueabihf-ar rcs pkg\lora\libpan3028.a pan3028.o pan3028_port.o spi-3028.o radio.o
+arm-linux-gnueabihf-gcc -c -o pan3028.o pan3028-app/pan3028.c 
+arm-linux-gnueabihf-gcc -c -o pan3028_port.o pan3028-app/pan3028_port.c
+arm-linux-gnueabihf-gcc -c -o spi-3028.o pan3028-app/spi-3028.c 
+arm-linux-gnueabihf-gcc -c -o radio.o pan3028-app/radio.c
+# .a must be in the same folder as the go file really using the .a lib
+arm-linux-gnueabihf-ar rcs pkg/lora/libpan3028.a pan3028.o pan3028_port.o spi-3028.o radio.o
