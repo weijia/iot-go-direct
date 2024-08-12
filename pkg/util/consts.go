eab2b9366e9e0d8872b336f70646d6aa05ea0999
weijiwan <weijiwan@cisco.com>
2024-08-12 10:02:49 +0800

    Update second level timeout

--- pkg/util/consts.go
+++ pkg/util/consts.go
@@ -4,7 +4,7 @@ const (
 	HEARTBEAT_REPLY_TIMEOUT                                = 10
 	GET_GLASS_STATUS_MAX_RETRY                             = 3
 	TO_SERVER_HEARTBEAT_SECONDS                            = 60
-	LEVEL2_NODE_MSG_REPLY_TIMEOUT_SECONDS                  = 10
+	LEVEL2_NODE_MSG_REPLY_TIMEOUT_SECONDS                  = 50
 	LEVEL1_WAIT_FOR_LORA_SERVICE_PUSH_DATA_TIMEOUT_SECONDS = 5
 	HEARTBEAT_RETRY_CNT                                    = 3
 	UPDATE_GLASS_COLOR_RETRY_CNT                           = 2
